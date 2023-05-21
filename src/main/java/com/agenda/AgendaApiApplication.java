832c4d8e9c545ae467d5290f2e2cf1e115ca81c8
igorsalgado <salgado.igor.is@gmail.com>
2023-05-21 19:45:32 -0300

    feat: Configurando solicitações CORS

--- src/main/java/com/agenda/AgendaApiApplication.java
+++ src/main/java/com/agenda/AgendaApiApplication.java
@@ -2,10 +2,12 @@ package com.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2 // Habilita o Swagger
+@EnableWebMvc
 public class AgendaApiApplication {
 
     public static void main(String[] args) {
